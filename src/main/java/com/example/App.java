7c5035dc58df5093083098e0e142e606bf5dd0ec
Javier <javi.ssweb@gmail.com>
2023-04-14 19:58:40 +0200

    proyecto base spring web asociaciones

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebC06AsociacionesEstudiosSanchiApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebC06AsociacionesEstudiosSanchiApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
